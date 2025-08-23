abf3c447d10a884d4a506fc38da630ee8561180f
kimdindoo <aaa119o1125@gmail.com>
2025-08-24 02:01:28 +0900

    feat: Add Domeggook API request

---
+++ lib/product/repository/product_repository.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:domeggook/product/model/product_model.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final dio = Dio();
+
+Future<ProductModel> getProduct() async {
+  try {
+    final Response response = await dio.get(
+      'https://domeggook.com/ssl/api',
+      queryParameters: {
+        'ver': '4.1',
+        'mode': 'getItemList',
+        'aid': dotenv.env['API_KEY'], // .env에 API_KEY 설정 필요
+        'market': 'dome',
+        'om': 'json',
+        'kw': '지갑',
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception(
+        'Request failed\n\nStatus Code: ${response.statusCode}.\nReason: ${response.statusMessage}',
+      );
+    }
+
+    if (response.data == null) {
+      throw Exception('Response data is null');
+    }
+
+    final product = ProductModel.fromJson(response.data);
+
+    print(product.domeggook);
+
+    return product;
+  } catch (e) {
+    rethrow;
+  }
+}
