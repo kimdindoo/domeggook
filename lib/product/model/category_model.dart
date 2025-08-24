70527755eea916471831a68698fe10f1b54bfae4
kimdindoo <aaa119o1125@gmail.com>
2025-08-25 01:09:21 +0900

    feat: Add category model

---
+++ lib/product/model/category_model.dart
@@ -0,0 +1,36 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'category_model.freezed.dart';
+part 'category_model.g.dart';
+
+/// Root JSON structure: { "domeggook": { "items": { ... } } }
+@freezed
+abstract class CategoryModel with _$CategoryModel {
+  const factory CategoryModel({required Domeggook domeggook}) = _CategoryModel;
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) =>
+      _$CategoryModelFromJson(json);
+}
+
+@freezed
+abstract class Domeggook with _$Domeggook {
+  const factory Domeggook({required Map<String, CategoryNode> items}) =
+      _Domeggook;
+
+  factory Domeggook.fromJson(Map<String, dynamic> json) =>
+      _$DomeggookFromJson(json);
+}
+
+@freezed
+abstract class CategoryNode with _$CategoryNode {
+  const factory CategoryNode({
+    required String code,
+    required String name,
+    String? locked,
+    @JsonKey(name: 'int') String? intValue, // JSON 필드명이 'int'여도 안전
+    Map<String, CategoryNode>? child,
+  }) = _CategoryNode;
+
+  factory CategoryNode.fromJson(Map<String, dynamic> json) =>
+      _$CategoryNodeFromJson(json);
+}
