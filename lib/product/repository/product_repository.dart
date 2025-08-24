efaa03f8a39b124aa95f96bf9af634c8519cba25
kimdindoo <aaa119o1125@gmail.com>
2025-08-25 00:52:56 +0900

    feat: Add product and category fetching methods with Dio

--- lib/product/repository/product_repository.dart
+++ lib/product/repository/product_repository.dart
@@ -1,11 +1,11 @@
-import 'dart:convert';
 import 'package:dio/dio.dart';
+import 'package:domeggook/product/model/category_model.dart';
 import 'package:domeggook/product/model/product_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final dio = Dio();
 
-Future<ProductModel> getProduct() async {
+Future<ProductModel> getProductByKeyword(String keyword) async {
   try {
     final Response response = await dio.get(
       'https://domeggook.com/ssl/api',
@@ -15,7 +15,7 @@ Future<ProductModel> getProduct() async {
         'aid': dotenv.env['API_KEY'], // .env에 API_KEY 설정 필요
         'market': 'dome',
         'om': 'json',
-        'kw': '지갑',
+        'kw': keyword,
       },
     );
 
@@ -34,6 +34,72 @@ Future<ProductModel> getProduct() async {
     print(product.domeggook);
 
     return product;
+  } catch (e) {
+    rethrow;
+  }
+}
+
+Future<ProductModel> getProductByCategory(String categoryNumber) async {
+  try {
+    final Response response = await dio.get(
+      'https://domeggook.com/ssl/api',
+      queryParameters: {
+        'ver': '4.1',
+        'mode': 'getItemList',
+        'aid': dotenv.env['API_KEY'], // .env에 API_KEY 설정 필요
+        'market': 'dome',
+        'om': 'json',
+        'ca': categoryNumber,
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception(
+        'Request failed\n\nStatus Code: ${response.statusCode}.\nReason: ${response.statusMessage}',
+      );
+    }
+
+    if (response.data == null) {
+      throw Exception('Response data is null');
+    }
+
+    final product = ProductModel.fromJson(response.data);
+
+    print(product.domeggook);
+
+    return product;
+  } catch (e) {
+    rethrow;
+  }
+}
+
+Future<CategoryModel> getCategory() async {
+  try {
+    final Response response = await dio.get(
+      'https://domeggook.com/ssl/api',
+      queryParameters: {
+        'ver': '1.0',
+        'mode': 'getCategoryList',
+        'aid': dotenv.env['API_KEY'], // .env에 API_KEY 설정 필요
+        'om': 'json',
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception(
+        'Request failed\n\nStatus Code: ${response.statusCode}.\nReason: ${response.statusMessage}',
+      );
+    }
+
+    if (response.data == null) {
+      throw Exception('Response data is null');
+    }
+
+    final category = CategoryModel.fromJson(response.data);
+
+    print(category);
+
+    return category;
   } catch (e) {
     rethrow;
   }
