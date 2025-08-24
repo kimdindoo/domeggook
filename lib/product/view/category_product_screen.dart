f7f92a324f07d1c2d0ba8e56aa15357982b027f8
kimdindoo <aaa119o1125@gmail.com>
2025-08-25 00:54:53 +0900

    feat: Add category product screen with product list and images

---
+++ lib/product/view/category_product_screen.dart
@@ -0,0 +1,61 @@
+import 'package:domeggook/common/layout/defalut_layout.dart';
+import 'package:domeggook/common/utils/number_utils.dart';
+import 'package:domeggook/product/model/product_model.dart';
+import 'package:domeggook/product/repository/product_repository.dart';
+import 'package:flutter/material.dart';
+
+class CategoryProductScreen extends StatelessWidget {
+  const CategoryProductScreen({
+    super.key,
+    required this.categoryName,
+    required this.categoryNumber,
+  });
+
+  final String categoryName;
+  final String categoryNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: categoryName,
+      child: SingleChildScrollView(
+        child: FutureBuilder<ProductModel>(
+          future: getProductByCategory(categoryNumber),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData ||
+                snapshot.data!.domeggook.list.item.isEmpty) {
+              return const Center(child: Text('No products found'));
+            }
+
+            final products = snapshot.data!.domeggook.list.item;
+
+            return ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                return ListTile(
+                  leading: Image.network(
+                    product.thumb,
+                    width: 100,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Text(product.title),
+                  subtitle: Text(
+                    '${NumberUtils.formatPrice(int.parse(product.price))}원',
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
