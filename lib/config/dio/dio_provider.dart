295f4feb62aec57b03a527c908d422131cf82796
kimdindoo <aaa119o1125@gmail.com>
2025-08-26 00:55:43 +0900

    feat: Add Dio provider

---
+++ lib/config/dio/dio_provider.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'dio_provider.g.dart';
+
+@riverpod
+Dio dio(Ref ref) {
+  final options = BaseOptions(baseUrl: 'https://domeggook.com/ssl/api/');
+  return Dio(options);
+}
