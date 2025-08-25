265b2983f9218cd823134bb484e6d9029f6ffccd
kimdindoo <aaa119o1125@gmail.com>
2025-08-25 20:21:38 +0900

    feat: Add ProductDetailScreen for displaying product details

---
+++ lib/product/view/product_detail_screen.dart
@@ -0,0 +1,11 @@
+import 'package:domeggook/common/layout/defalut_layout.dart';
+import 'package:flutter/widgets.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  const ProductDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(title: '상품 상세', child: Container());
+  }
+}
