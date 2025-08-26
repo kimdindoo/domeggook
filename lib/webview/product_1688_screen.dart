4ab0c9dfdb05842f8715321f760d9f65ce5bc183
kimdindoo <aaa119o1125@gmail.com>
2025-08-27 03:02:43 +0900

    feat: Add Product1688Screen with WebView

---
+++ lib/webview/product_1688_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Product1688Screen extends StatefulWidget {
+  const Product1688Screen({super.key});
+
+  @override
+  State<Product1688Screen> createState() => _Product1688ScreenState();
+}
+
+class _Product1688ScreenState extends State<Product1688Screen> {
+  WebViewController? _webViewController;
+
+  @override
+  void initState() {
+    _webViewController = WebViewController()
+      ..loadRequest(Uri.parse('https://1688.domeggook.com/'))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.black,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: WebViewWidget(controller: _webViewController!),
+    );
+  }
+}
