db0f287e75e8d8c36731594c983265f7ea4eb7ff
kimdindoo <aaa119o1125@gmail.com>
2025-08-26 17:36:32 +0900

    feat: Add CustomInterceptor to Dio provider for request/response logging

--- lib/config/dio/dio_provider.dart
+++ lib/config/dio/dio_provider.dart
@@ -7,5 +7,37 @@ part 'dio_provider.g.dart';
 @riverpod
 Dio dio(Ref ref) {
   final options = BaseOptions(baseUrl: 'https://domeggook.com/ssl/api/');
-  return Dio(options);
+
+  final dio = Dio(options);
+
+  // 여기서 CustomInterceptor 추가
+  dio.interceptors.add(CustomInterceptor(accessToken: null));
+
+  return dio;
+}
+
+class CustomInterceptor extends Interceptor {
+  final String? accessToken;
+
+  CustomInterceptor({this.accessToken});
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    print('[REQ] [${options.method}] ${options.uri}');
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    print(
+      '[RES] [${response.requestOptions.method}] ${response.requestOptions.uri}',
+    );
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    print('[ERR] [${err.requestOptions.method}] ${err.requestOptions.uri}');
+    super.onError(err, handler);
+  }
 }
