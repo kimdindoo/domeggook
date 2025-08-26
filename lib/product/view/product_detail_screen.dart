0fb258c4bdb00f1f9a9f5fb07497c205c204101b
kimdindoo <aaa119o1125@gmail.com>
2025-08-27 03:19:44 +0900

    feat: Add Riverpod provider to ProductDetailScreen

--- lib/product/view/product_detail_screen.dart
+++ lib/product/view/product_detail_screen.dart
@@ -1,16 +1,42 @@
 import 'package:domeggook/common/layout/defalut_layout.dart';
-import 'package:flutter/widgets.dart';
+import 'package:domeggook/product/provider/product_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends ConsumerWidget {
   const ProductDetailScreen({super.key, required this.productNo});
 
   final String productNo;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productDetail = ref.watch(productDetailProvider(productNo));
     return DefaultLayout(
       title: '상품 상세',
-      child: Center(child: Text('상품 번호: $productNo')),
+      child: Center(
+        child: Column(
+          children: [
+            productDetail.when(
+              data: (data) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // 이미지 추가
+                    Image.network(data.domeggook.thumb.large!),
+                    const SizedBox(height: 16),
+                    Text('${data.domeggook.basis.title}'),
+                    // Text('가격: ${formatDomePrice(data.domeggook.price.dome)}'),
+                    // Text('최소구매수량: ${data.domeggook.qty.domeMoq}개'),
+                    // Text('배송비: ${data.domeggook.deli.feeExtra}원'),
+                  ],
+                );
+              },
+              loading: () => const CircularProgressIndicator(),
+              error: (err, _) => Text('Error: $err'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
