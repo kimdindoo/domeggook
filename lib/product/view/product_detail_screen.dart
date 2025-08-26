75964eb8f098015534958cfab56b6a3dff4910c9
kimdindoo <aaa119o1125@gmail.com>
2025-08-26 17:41:30 +0900

    feat: Add ProductDetailScreen

--- lib/product/view/product_detail_screen.dart
+++ lib/product/view/product_detail_screen.dart
@@ -2,10 +2,15 @@ import 'package:domeggook/common/layout/defalut_layout.dart';
 import 'package:flutter/widgets.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  const ProductDetailScreen({super.key});
+  const ProductDetailScreen({super.key, required this.productNo});
+
+  final String productNo;
 
   @override
   Widget build(BuildContext context) {
-    return DefaultLayout(title: '상품 상세', child: Container());
+    return DefaultLayout(
+      title: '상품 상세',
+      child: Center(child: Text('상품 번호: $productNo')),
+    );
   }
 }
