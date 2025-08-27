b9d1b760598693dc5d9ee5dba007f88cfb21698f
kimdindoo <aaa119o1125@gmail.com>
2025-08-27 23:53:11 +0900

    feat: Add home main screen

---
+++ lib/home/view/home_main_screen.dart
@@ -0,0 +1,11 @@
+import 'package:domeggook/home/component/slide_image.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeMainScreen extends StatelessWidget {
+  const HomeMainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [SlideImage()]);
+  }
+}
