8d36ae14dd84edfc09faa879653bf29e687ff879
kimdindoo <aaa119o1125@gmail.com>
2025-08-27 23:52:05 +0900

    feat: Add route name

--- lib/config/router/route_names.dart
+++ lib/config/router/route_names.dart
@@ -6,4 +6,7 @@ class RouteNames {
   static const String keywordProduct = 'keywordProduct';
   static const String productDetail = 'productDetail';
   static const String product1688 = 'product1688';
+  static const String recentlyViewedProduct = 'recentlyViewedProduct';
+  static const String splash = 'splash';
+  static const String error = 'error';
 }
