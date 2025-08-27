50b170e53ca3a04fe42303aa1b285435d98e78a6
kimdindoo <aaa119o1125@gmail.com>
2025-08-27 23:50:09 +0900

    feat: Add error screen

---
+++ lib/common/view/error_screen.dart
@@ -0,0 +1,43 @@
+import 'package:domeggook/config/router/route_names.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class ErrorScreen extends ConsumerWidget {
+  const ErrorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.error_outline, size: 64, color: Colors.red),
+              const SizedBox(height: 16),
+              Text(
+                '오류가 발생했습니다.\n잠시 후 다시 시도해주세요.',
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () async {
+                  GoRouter.of(context).goNamed(RouteNames.splash);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  side: const BorderSide(color: Colors.black),
+                ),
+                child: const Text('홈 화면으로 이동'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
