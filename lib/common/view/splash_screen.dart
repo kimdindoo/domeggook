92e014076de884acf3ed8cb9bb3d88335eff1c97
kimdindoo <aaa119o1125@gmail.com>
2025-08-27 23:50:00 +0900

    feat: Add splash screen

---
+++ lib/common/view/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:domeggook/category/provider/category_provider.dart';
+import 'package:domeggook/common/layout/defalut_layout.dart';
+import 'package:domeggook/config/router/route_names.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends ConsumerStatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    try {
+      // 카테고리 데이터 미리 로드
+      await ref.read(categoryProvider.future);
+
+      // 데이터 로드 후 홈 화면으로 이동
+      if (!mounted) return; // 위젯이 여전히 화면에 있을 때만 이동
+      GoRouter.of(context).goNamed(RouteNames.home);
+    } catch (e) {
+      debugPrint('Splash load error: $e');
+      // 필요하다면 에러 화면 또는 홈 이동
+      if (!mounted) return;
+
+      GoRouter.of(context).goNamed(RouteNames.error);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'asset/img/logo/domeggook_logo.png',
+              width: MediaQuery.of(context).size.width / 2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
