225d7ac8d703b390e8b9297afbc86eec7523e9fc
kimdindoo <aaa119o1125@gmail.com>
2025-08-28 14:28:04 +0900

    refactor: Remove custom webview handling, directly display 1688 screen in TabBarView.

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,19 +1,44 @@
+import 'package:domeggook/common/layout/defalut_layout.dart';
 import 'package:domeggook/config/router/route_names.dart';
+import 'package:domeggook/home/view/home_event_screen.dart';
+import 'package:domeggook/home/view/home_main_screen.dart';
+import 'package:domeggook/webview/product_1688_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
+    return SafeArea(
+      child: DefaultLayout(
         child: DefaultTabController(
           length: 3,
           child: CustomScrollView(
             slivers: [
               SliverAppBar(
+                backgroundColor: Colors.white,
                 snap: false,
                 pinned: false,
                 floating: false,
@@ -28,15 +53,9 @@ class HomeScreen extends StatelessWidget {
                 ),
                 title: GestureDetector(
                   onTap: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (_) =>
-                    //         const RootTab(initialIndex: 2), // 2번 탭 = 검색
-                    //   ),
-                    // );
-
-                    GoRouter.of(context).pushNamed(RouteNames.productSearch);
+                    GoRouter.of(
+                      context,
+                    ).goNamed(RouteNames.productSearch); // 검색 화면으로 이동
                   },
                   child: Container(
                     width: double.infinity,
@@ -47,43 +66,37 @@ class HomeScreen extends StatelessWidget {
                       borderRadius: BorderRadius.circular(20),
                       border: Border.all(color: Colors.grey.shade300),
                     ),
-                    child: Row(
+                    child: const Row(
                       children: [
-                        const SizedBox(width: 8),
+                        SizedBox(width: 8),
                         Expanded(
                           child: AbsorbPointer(
-                            // TextField 클릭 방지
                             child: TextField(
                               readOnly: true,
-                              decoration: const InputDecoration(
-                                // hintText: "검색어를 입력하세요",
+                              decoration: InputDecoration(
                                 border: InputBorder.none,
                                 isDense: true,
                               ),
                             ),
                           ),
                         ),
-                        const Icon(Icons.search, color: Colors.grey),
+                        Icon(Icons.search, color: Colors.grey),
                       ],
                     ),
                   ),
                 ),
-                // actions: [
-                //   IconButton(
-                //     icon: const Icon(Icons.shopping_cart_outlined),
-                //     onPressed: () {
-                //       // 장바구니 아이콘 클릭 시 동작
-                //     },
-                //   ),
-                // ],
               ),
-              SliverPersistentHeader(pinned: true, delegate: TabBarDelegate()),
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: TabBarDelegate(tabController: _tabController),
+              ),
               SliverFillRemaining(
                 child: TabBarView(
-                  children: [
-                    Center(child: Text('홈 화면')),
-                    Center(child: Text('1688상품권 화면')),
-                    Center(child: Text('이벤트 화면')),
+                  controller: _tabController,
+                  children: const [
+                    HomeMainScreen(),
+                    Product1688Screen(),
+                    HomeEventScreen(),
                   ],
                 ),
               ),
@@ -96,7 +109,8 @@ class HomeScreen extends StatelessWidget {
 }
 
 class TabBarDelegate extends SliverPersistentHeaderDelegate {
-  const TabBarDelegate();
+  final TabController tabController;
+  const TabBarDelegate({required this.tabController});
 
   @override
   Widget build(
@@ -107,14 +121,16 @@ class TabBarDelegate extends SliverPersistentHeaderDelegate {
     return Container(
       color: Colors.white,
       child: TabBar(
-        tabs: [
-          Tab(child: const Text('홈')),
-          Tab(child: const Text('1688상품권')),
-          Tab(child: const Text('이벤트')),
+        controller: tabController,
+        tabs: const [
+          Tab(child: Text('홈')),
+          Tab(
+            child: Text('1688상품관', style: TextStyle(color: Colors.red)),
+          ),
+          Tab(child: Text('이벤트')),
         ],
         indicatorWeight: 2,
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-
         labelColor: Colors.black,
         indicatorColor: Colors.black,
         indicatorSize: TabBarIndicatorSize.label,
@@ -124,12 +140,9 @@ class TabBarDelegate extends SliverPersistentHeaderDelegate {
 
   @override
   double get maxExtent => 48;
-
   @override
   double get minExtent => 48;
-
   @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
-  }
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      false;
 }
