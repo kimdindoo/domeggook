b87e5c2149446661c44df0cdd955833f8f6f1767
kimdindoo <aaa119o1125@gmail.com>
2025-08-28 13:12:13 +0900

    feat: Add additional features to recent searches

--- lib/product/view/product_search_screen.dart
+++ lib/product/view/product_search_screen.dart
@@ -1,34 +1,39 @@
 import 'package:domeggook/common/layout/defalut_layout.dart';
 import 'package:domeggook/config/router/route_names.dart';
+import 'package:domeggook/product/component/recent_search.dart';
+import 'package:domeggook/product/component/recent_viewd_product.dart';
+import 'package:domeggook/product/provider/recent_search_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class ProductSearchScreen extends StatefulWidget {
+class ProductSearchScreen extends ConsumerStatefulWidget {
   const ProductSearchScreen({super.key});
 
   @override
-  State<ProductSearchScreen> createState() => _ProductSearchScreenState();
+  ConsumerState<ProductSearchScreen> createState() =>
+      _ProductSearchScreenState();
 }
 
-class _ProductSearchScreenState extends State<ProductSearchScreen> {
+class _ProductSearchScreenState extends ConsumerState<ProductSearchScreen> {
   final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '상품 검색',
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            Container(
               height: 40,
-              padding: const EdgeInsets.symmetric(horizontal: 8),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
                 border: Border.all(color: Colors.grey.shade300),
               ),
+
               child: Row(
                 children: [
                   const SizedBox(width: 8),
@@ -44,6 +49,11 @@ class _ProductSearchScreenState extends State<ProductSearchScreen> {
                       onSubmitted: (String value) {
                         if (value.isEmpty) return;
 
+                        ref
+                            .read(recentSearcheProvider.notifier)
+                            .addSearch(value); // 최근 검색어에 추가
+
+                        // 검색 결과 페이지로 이동
                         GoRouter.of(context).pushNamed(
                           RouteNames.keywordProduct,
                           queryParameters: {'keyword': value},
@@ -55,8 +65,12 @@ class _ProductSearchScreenState extends State<ProductSearchScreen> {
                 ],
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 16),
+            RecentSearchWidget(),
+            const SizedBox(height: 16),
+            RecentViewdProduct(),
+          ],
+        ),
       ),
     );
   }
